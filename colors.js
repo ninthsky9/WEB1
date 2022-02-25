25320854d7df36046a82e1f03985fcdea474a7e1
ninthsky9 <60373068+ninthsky9@users.noreply.github.com>
2022-02-25 16:12:25 +0900

    jQuery추가

--- colors.js
+++ colors.js
@@ -2,20 +2,22 @@
 var Links = {
   setColor:function (color){
     var alist = document.querySelectorAll('a');
-    var i = 0;
-    while(i < alist.length){
-      alist[i].style.color = color;
-      i++;
-    }
+    // var i = 0;
+    // while(i < alist.length){
+    //   alist[i].style.color = color;
+    //   i++;
+    // } 선택 후 ctrl+slash - 주석화
+    $('a').css('color', color); // 모든 a 태그를 jquery로 제어하겠다
   }
 }
 var Body = {
   setColor:function (color){
-    document.querySelector('body').style.color = color;
+    //document.querySelector('body').style.color = color;
+    $('body').css('color',color);
   },
   setBackgroundColor:function (color){
-    document.querySelector('body').style.backgroundColor = color;
-    return 1
+    //document.querySelector('body').style.backgroundColor = color;
+    $('body').css('backgroundColor',color);
   }
 }
 function nightDayHandler(self){
